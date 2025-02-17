69fd2529a2701831170dd97338d61bc8aa6e128e
Cynchronized <austin.thieu123@gmail.com>
2025-02-17 12:36:51 -0700

    complete paintings API routes

--- controllers/paintings.ts
+++ controllers/paintings.ts
@@ -7,8 +7,8 @@ const paintingRouter = Router();
 const defaultQuery = `
     paintingId, imageFileName, title, museumLink, accessionNumber, copyrightText, description,
     excerpt, yearOfWork, width, height, medium, cost, MSRP, googleLink, googleDescription, wikiLink, jsonAnnotations,
-    artists (firstName, lastName, nationality, gender, yearOfBirth, yearOfDeath, details, artistLink), 
-    galleries (galleryName, galleryNativeName, galleryCity, galleryAddress, galleryCountry, latitude, longitude, galleryWebSite, flickrPlaceId, yahooWoeId, googlePlaceId)
+    artists!inner (firstName, lastName, nationality, gender, yearOfBirth, yearOfDeath, details, artistLink), 
+    galleries!inner (galleryName, galleryNativeName, galleryCity, galleryAddress, galleryCountry, latitude, longitude, galleryWebSite, flickrPlaceId, yahooWoeId, googlePlaceId)
   `;
 
 paintingRouter.get('/', async (req: Request, res: Response) => {
@@ -20,7 +20,87 @@ paintingRouter.get('/', async (req: Request, res: Response) => {
   res.status(200).json(data);
 });
 
-// TODO: Rest of API calls
+paintingRouter.get('/sort/:sortBy', async (req: Request, res: Response) => {
+  const { sortBy } = req.params;
+  const validSortFields = ['title', 'yearOfWork'];
 
+  if (!validSortFields.includes(sortBy)) {
+    res.status(400).json({ error: 'Invalid sort parameter. Use "title" or "year".' });
+  }
+
+  const { data, error } = await supabase
+    .from('paintings')
+    .select(`${defaultQuery}`)
+    .order(sortBy);
+
+  res.status(200).json(data);
+});
+
+paintingRouter.get('/:ref', async (req: Request, res: Response) => {
+  const { data, error } = await supabase
+    .from('paintings')
+    .select(`${defaultQuery}`)
+    .eq('paintingId', req.params.ref)
+    .order('title');
+
+  res.status(200).json(data);
+});
+
+paintingRouter.get('/search/:substring', async (req: Request, res: Response) => {
+  const { data, error } = await supabase
+    .from('paintings')
+    .select(`${defaultQuery}`)
+    .ilike('title', `${req.params.substring}%`)
+    .order('title');
+
+  res.status(200).json(data);
+});
+
+paintingRouter.get('/years/:start/:end', async (req: Request, res: Response) => {
+  const { start, end } = req.params
+
+  if (end < start) {
+    res.status(400).json({ error: 'ending year must be greater than start year' })
+  } else {
+    const { data, error } = await supabase
+      .from('paintings')
+      .select(`${defaultQuery}`)
+      .gte('yearOfWork', start)
+      .lte('yearOfWork', end)
+      .order('yearOfWork');
+
+    res.status(200).json(data);
+  }
+});
+
+paintingRouter.get('/galleries/:ref', async (req: Request, res: Response) => {
+  const { data, error } = await supabase
+    .from('paintings')
+    .select(`${defaultQuery}`)
+    .eq('galleries.galleryId', req.params.ref)
+    .order('title');
+
+  res.status(200).json(data);
+});
+
+paintingRouter.get('/artist/:ref', async (req: Request, res: Response) => {
+  const { data, error } = await supabase
+    .from('paintings')
+    .select(`${defaultQuery}`)
+    .eq('artists.artistId', req.params.ref)
+    .order('title');
+
+  res.status(200).json(data);
+});
+
+paintingRouter.get('/artists/country/:substring', async (req: Request, res: Response) => {
+  const { data, error } = await supabase
+    .from('paintings')
+    .select(`${defaultQuery}`)
+    .ilike('artists.country', `${req.params.substring}%`)
+    .order('title')
+
+  res.status(200).json(data);
+});
 
 export default paintingRouter;
